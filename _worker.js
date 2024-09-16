69d6b053624b65d94bb8335e07c23159b6dab82e
i3acixu9 <i3acixu9@proton.me>
2024-09-16 11:29:09 +0800

    worker.js

--- _worker.js
+++ _worker.js
@@ -1116,18 +1116,18 @@ function generateRemark(index, port, protocol, fragType) {
     switch (index) {
         case 0:
         case 1:
-            remark = `💦 ${protocol}${type} - Domain ${index + 1} : ${port}`;
+            remark = `${protocol}${type} - Domain ${index + 1} : ${port}`;
             break;
         case 2:
         case 3:
-            remark = `💦 ${protocol}${type} - IPv4 ${index - 1} : ${port}`;
+            remark = `${protocol}${type} - IPv4 ${index - 1} : ${port}`;
             break;
         case 4:
         case 5:
-            remark = `💦 ${protocol}${type} - IPv6 ${index - 3} : ${port}`;
+            remark = `${protocol}${type} - IPv6 ${index - 3} : ${port}`;
             break;
         default:
-            remark = `💦 ${protocol}${type} - Clean IP ${index - 5} : ${port}`;
+            remark = `${protocol}${type} - Clean IP ${index - 5} : ${port}`;
             break;
     }
 
